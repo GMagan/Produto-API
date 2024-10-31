a90a2d965de6c5aaf05b5da157047ff4071656d8
GMagan <guilherme_magan@hotmail.com>
2024-10-30 22:13:02 -0300

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require("express");
+const productsRouter = require('./routes/products');
+const app = express();
+
+app.use(express.json());
+app.use('/produtos', productsRouter);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+   console.log("Server up in " + PORT);
+});
