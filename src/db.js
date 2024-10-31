0731a88ab0315f7c5dd77bd4a87feaa362bdb45f
GMagan <guilherme_magan@hotmail.com>
2024-10-30 22:12:59 -0300

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
+
+const pool = new Pool({
+   user: process.env.PGUSER,
+   host: process.env.PGHOST,
+   database: process.env.PGDATABASE,
+   password: process.env.PGPASSWORD,
+   port: process.env.PGPORT
+});
+
+const runScript = async (filePath) => {
+   const script = fs.readFileSync(path.join(__dirname, filePath)).toString();
+   try {
+      await pool.query(script);
+      console.log(`${filePath} running`);
+   } catch (error) {
+      console.error(`Error in ${filePath}:`, error.message);
+   }
+};
+
+const runAllScripts = async () => {
+   await runScript('database/createTable.sql');
+   await runScript('database/seed.sql');
+};
+
+runAllScripts();
+
+module.exports = pool;
